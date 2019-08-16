05e218656720f98e65f9c61c9c4f2ff71d5f846b
Adam Antinoo <adamantinoo.git@gmail.com>
2019-08-17 00:38:34 +0200

    -[MVC] Controller factories should be singletons so the creation of one will allow also the use from static space like the usage from Activities.

--- AndroidMVC/src/main/java/org/dimensinfin/android/mvc/controller/ControllerFactory.java
+++ AndroidMVC/src/main/java/org/dimensinfin/android/mvc/controller/ControllerFactory.java
@@ -1,27 +1,25 @@
 package org.dimensinfin.android.mvc.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
 import android.content.Context;
 import android.content.Intent;
 
 import org.dimensinfin.android.mvc.exception.ExceptionReport;
-import org.dimensinfin.android.mvc.exception.MVCExceptionHandler;
 import org.dimensinfin.android.mvc.interfaces.IControllerFactory;
 import org.dimensinfin.android.mvc.support.SeparatorController;
 import org.dimensinfin.core.interfaces.ICollaboration;
 import org.dimensinfin.core.model.Separator;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class ControllerFactory implements IControllerFactory {
-	protected static Logger logger = LoggerFactory.getLogger(ControllerFactory.class);
+	protected static final Logger logger = LoggerFactory.getLogger(ControllerFactory.class);
+	private static final Map<String, Class> activityRegistry = new HashMap<>();
 
 	// - F I E L D - S E C T I O N
-	private Map<String, Class> activityRegistry = new HashMap();
 	private final String variant;
 
 	// - C O N S T R U C T O R - S E C T I O N
@@ -36,9 +34,6 @@ public class ControllerFactory implements IControllerFactory {
 			return new SeparatorController((Separator) node, this)
 					       .setRenderMode(this.getVariant());
 		}
-//		if (node instanceof MVCExceptionHandler.ExceptionModel) {
-//			return new ExceptionController((MVCExceptionHandler.ExceptionModel) node, this);
-//		}
 		if (node instanceof ExceptionReport) {
 			return new ExceptionController((ExceptionReport) node, this);
 		}
@@ -47,21 +42,19 @@ public class ControllerFactory implements IControllerFactory {
 				, this);
 	}
 
+	// - G E T T E R S   &   S E T T E R S
+	public String getVariant() {
+		return variant;
+	}
 
 	public IControllerFactory registerActivity( final String activityCode, final Class activityClass ) {
-		this.activityRegistry.put(activityCode, activityClass);
+		activityRegistry.put(activityCode, activityClass);
 		return this;
 	}
 
 	public Intent prepareActivity( final String activityCode, final Context context ) {
-		final Class activity = this.activityRegistry.get(activityCode);
+		final Class activity = activityRegistry.get(activityCode);
 		Objects.requireNonNull(activity);
-		final Intent intent = new Intent(context, activity);
-		return intent;
-	}
-
-	// - G E T T E R S   &   S E T T E R S
-	public String getVariant() {
-		return variant;
+		return new Intent(context, activity);
 	}
 }
