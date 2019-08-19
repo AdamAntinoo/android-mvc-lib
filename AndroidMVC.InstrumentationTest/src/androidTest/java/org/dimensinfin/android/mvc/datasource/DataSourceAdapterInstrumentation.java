eeb82b6ecffa41f53074191e6b6c274aa9fe9f9b
Adam Antinoo <adamantinoo.git@gmail.com>
2019-08-19 13:51:44 +0200

    -[TESTING] Coverage does not account for library code.

--- AndroidMVC.InstrumentationTest/src/androidTest/java/org/dimensinfin/android/mvc/datasource/DataSourceAdapterInstrumentation.java
+++ AndroidMVC.InstrumentationTest/src/androidTest/java/org/dimensinfin/android/mvc/datasource/DataSourceAdapterInstrumentation.java
@@ -4,17 +4,17 @@ import android.view.View;
 
 import androidx.test.rule.ActivityTestRule;
 
-import org.dimensinfin.android.mvc.activity.InstrumentationActivity;
+import org.dimensinfin.android.mvc.instrumentation.activity.AcceptanceActivity00;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
 //@RunWith(AndroidJUnit4.class)
 public class DataSourceAdapterInstrumentation {
-	private InstrumentationActivity activity;
-	private InstrumentationActivity.InstrumentationFragment fragment;
+	private AcceptanceActivity00 activity;
+	private AcceptanceActivity00.MVC00Fragment fragment;
 	@Rule
-	public ActivityTestRule<InstrumentationActivity> activityRule = new ActivityTestRule<>(InstrumentationActivity.class,
+	public ActivityTestRule<AcceptanceActivity00> activityRule = new ActivityTestRule<>(AcceptanceActivity00.class,
 	                                                                                         false, false);
 
 	@Test
@@ -23,9 +23,9 @@ public class DataSourceAdapterInstrumentation {
 		this.activityRule.launchActivity(null);
 		this.activity = this.activityRule.getActivity();
 		Assert.assertNotNull(this.activity);
-		this.fragment = (InstrumentationActivity.InstrumentationFragment) this.activity.accessPageAdapter().getItem(0);
+		this.fragment = (AcceptanceActivity00.MVC00Fragment) this.activity.accessPageAdapter().getItem(0);
 		Assert.assertNotNull(this.fragment);
-		final View panel = this.fragment.accessDataSectionContainer().getChildAt(0);
+		final View panel = this.fragment.accessHeaderContainer().getChildAt(0);
 		Assert.assertNotNull(panel);
 	}
 }
