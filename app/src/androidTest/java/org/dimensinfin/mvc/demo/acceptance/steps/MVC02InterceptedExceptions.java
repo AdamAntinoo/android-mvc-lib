53371631c936c6a0db925b87014fb039221f57bc
Adam Antinoo <adamantinoo.git@gmail.com>
2020-04-01 18:57:53 +0200

    -[TESTING] Fixing old acceptance and unit tests that were reincorporated to the project.

--- app/src/androidTest/java/org/dimensinfin/mvc/demo/acceptance/steps/MVC02InterceptedExceptions.java
+++ app/src/androidTest/java/org/dimensinfin/mvc/demo/acceptance/steps/MVC02InterceptedExceptions.java
@@ -1,37 +1,27 @@
-package org.dimensinfin.mvc.demo.acceptance.test.steps;
+package org.dimensinfin.mvc.demo.acceptance.steps;
 
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+import androidx.fragment.app.Fragment;
 import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.util.HumanReadables;
-import androidx.test.rule.ActivityTestRule;
 
-import org.dimensinfin.mvc.demo.acceptance.R;
-import org.dimensinfin.mvc.demo.acceptance.activity.AcceptanceActivity02;
-import org.dimensinfin.mvc.demo.acceptance.activity.AcceptanceActivity04;
-import org.dimensinfin.mvc.demo.acceptance.activity.MVC02Fragment;
-import org.dimensinfin.mvc.demo.acceptance.test.support.MVCWorld;
 import org.junit.Assert;
 
-import cucumber.api.java.After;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import org.dimensinfin.mvc.demo.acceptance.support.core.MVCWorld;
 
-import static androidx.test.espresso.Espresso.onView;
+import cucumber.api.java.en.And;
+
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 public class MVC02InterceptedExceptions {
-	private static final String EXCEPTION_MESSAGE ="The data source cannot be a null reference. Please review the fragment code" +
-			                                               " and implement the 'createDS' method.";
+	private static final String EXCEPTION_MESSAGE = "The data source cannot be a null reference. Please review the fragment code" +
+			                                                " and implement the 'createDS' method.";
 	private MVCWorld world;
-	private AcceptanceActivity02 activity;
-	private MVC02Fragment fragment;
+	//	private AcceptanceActivity02 activity;
+	private Fragment fragment;
 
 //	private ActivityTestRule<AcceptanceActivity02> mActivityRule = new ActivityTestRule<>(AcceptanceActivity02.class, false,
 //	                                                                                      false);
@@ -53,9 +43,9 @@ public class MVC02InterceptedExceptions {
 		return new ViewAssertion() {
 			@Override
 			public void check( View view, NoMatchingViewException noView ) {
-				if (view != null && isDisplayed().matches(view)) {
-					throw new AssertionError("View is present in the hierarchy and Displayed: "
-							                         + HumanReadables.describe(view));
+				if (view != null && isDisplayed().matches( view )) {
+					throw new AssertionError( "View is present in the hierarchy and Displayed: "
+							                          + HumanReadables.describe( view ) );
 				}
 			}
 		};
@@ -70,23 +60,23 @@ public class MVC02InterceptedExceptions {
 //		Assert.assertEquals(activityClass, this.activity.getClass().getSimpleName());
 //	}
 
-	@And("a Fragment that do not defines a DataSource")
-	public void aFragmentThatDoNotDefinesADataSource() {
-		this.activity = (AcceptanceActivity02) this.world.getActiveActivity();
-		Assert.assertNotNull(this.activity);
-		this.fragment = (MVC02Fragment) this.activity.accessPageAdapter().getItem(0);
-		Assert.assertNotNull(fragment);
-	}
+//	@And("a Fragment that do not defines a DataSource")
+//	public void aFragmentThatDoNotDefinesADataSource() {
+//		this.activity = (AcceptanceActivity02) this.world.getActiveActivity();
+//		Assert.assertNotNull(this.activity);
+//		this.fragment = (MVC02Fragment) this.activity.accessPageAdapter().getItem(0);
+//		Assert.assertNotNull(fragment);
+//	}
 
-	@When("reaching the onResume phase")
-	public void reachingTheOnResumePhase() {
-		Assert.assertTrue(this.activity.isOnResumeReached());
-	}
-
-	@Then("get an exception at the onCreateView phase")
-	public void getAnExceptionAtTheOnCreateViewPhase() {
-		Assert.assertNotNull(this.fragment.getLastException());
-	}
+//	@When("reaching the onResume phase")
+//	public void reachingTheOnResumePhase() {
+//		Assert.assertTrue(this.activity.isOnResumeReached());
+//	}
+//
+//	@Then("get an exception at the onCreateView phase")
+//	public void getAnExceptionAtTheOnCreateViewPhase() {
+//		Assert.assertNotNull(this.fragment.getLastException());
+//	}
 
 	@And("deactivate data panels")
 	public void deactivateDataPanels() {
@@ -96,14 +86,15 @@ public class MVC02InterceptedExceptions {
 
 	@And("render exception to page")
 	public void renderExceptionToPage() {
+		// TODO - Obsolete test code
 		final ViewGroup exceptionContainer =
-				this.fragment.getView().findViewById(org.dimensinfin.android.mvc.R.id.exceptionContainer);
-		Assert.assertEquals(1,exceptionContainer.getChildCount());
-		final View exceptionView = exceptionContainer.getChildAt(0);
-		Assert.assertNotNull(exceptionView);
+				this.fragment.getView().findViewById( org.dimensinfin.android.mvc.R.id.exceptionContainer );
+		Assert.assertEquals( 1, exceptionContainer.getChildCount() );
+		final View exceptionView = exceptionContainer.getChildAt( 0 );
+		Assert.assertNotNull( exceptionView );
 		final String expected = EXCEPTION_MESSAGE;
 		final String obtained =
-				((TextView) exceptionView.findViewById(org.dimensinfin.android.mvc.R.id.exceptionMessage)).getText().toString();
-		Assert.assertEquals(expected,obtained);
+				((TextView) exceptionView.findViewById( org.dimensinfin.android.mvc.R.id.exceptionMessage )).getText().toString();
+		Assert.assertEquals( expected, obtained );
 	}
 }
--- app/src/androidTest/java/org/dimensinfin/mvc/demo/acceptance/steps/MVC04RunDataSource.java
+++ app/src/androidTest/java/org/dimensinfin/mvc/demo/acceptance/steps/MVC04RunDataSource.java
@@ -1,25 +1,20 @@
-package org.dimensinfin.mvc.demo.acceptance.test.steps;
-
-import androidx.test.rule.ActivityTestRule;
-
-import org.dimensinfin.mvc.demo.acceptance.activity.AcceptanceActivity01;
-import org.dimensinfin.mvc.demo.acceptance.activity.AcceptanceActivity04;
-import org.dimensinfin.mvc.demo.acceptance.activity.AcceptanceControllerFactory;
-import org.dimensinfin.mvc.demo.acceptance.activity.MVC01Fragment;
-import org.dimensinfin.mvc.demo.acceptance.activity.MVC04Fragment;
-import org.dimensinfin.mvc.demo.acceptance.datasource.MVC04DataSource;
-import org.dimensinfin.mvc.demo.acceptance.test.support.MVCWorld;
-import org.dimensinfin.android.mvc.controller.IAndroidController;
-import org.dimensinfin.android.mvc.datasource.DataSourceManager;
-import org.dimensinfin.android.mvc.datasource.IDataSource;
-import org.dimensinfin.android.mvc.domain.Spacer;
-import org.junit.Assert;
+package org.dimensinfin.mvc.demo.acceptance.steps;
 
 import java.util.List;
 import java.util.Map;
 
+import org.junit.Assert;
+import org.mockito.Mockito;
+
+import org.dimensinfin.android.mvc.controller.ControllerFactory;
+import org.dimensinfin.android.mvc.controller.IAndroidController;
+import org.dimensinfin.android.mvc.datasource.DataSourceManager;
+import org.dimensinfin.android.mvc.datasource.IDataSource;
+import org.dimensinfin.android.mvc.datasource.MVCDataSource;
+import org.dimensinfin.android.mvc.domain.Spacer;
+import org.dimensinfin.mvc.demo.acceptance.support.core.MVCWorld;
+
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -28,8 +23,8 @@ public class MVC04RunDataSource {
 	private static final String CONTROLLER_MODEL_TITLE = "controllerModelTitle";
 
 	private MVCWorld world;
-	private AcceptanceActivity04 activity;
-	private MVC04Fragment fragment;
+	//	private AcceptanceActivity04 activity;
+//	private MVC04Fragment fragment;
 	private MVC04DataSource dataSource;
 	private IDataSource registeredDataSouce;
 
@@ -39,33 +34,34 @@ public class MVC04RunDataSource {
 
 	@And("a new data source")
 	public void aNewDataSource() {
-		this.activity = (AcceptanceActivity04) this.world.getActiveActivity();
-		Assert.assertNotNull(this.activity);
+		// TODO - This is already osolete because the new acceptance test definitions
+//		this.activity = (AcceptanceActivity04) this.world.getActiveActivity();
+//		Assert.assertNotNull(this.activity);
 //		this.mActivityRule.launchActivity(null);
 //		this.activity = this.mActivityRule.getActivity();
 //		Assert.assertNotNull(this.activity);
-		this.fragment = (MVC04Fragment) this.activity.accessPageAdapter().getItem(0);
-		Assert.assertNotNull(this.fragment);
-
+//		this.fragment = (MVC04Fragment) this.activity.accessPageAdapter().getItem(0);
+//		Assert.assertNotNull(this.fragment);
+		final ControllerFactory factory = Mockito.mock( ControllerFactory.class );
 		final String variant = "-MVC04RUNDATASOURCE-";
 		this.dataSource = new MVC04DataSource.Builder()
-				                  .addIdentifier("-MVC04-")
-				                  .withVariant(variant)
-				                  .withFactory(new AcceptanceControllerFactory(variant))
+				                  .addIdentifier( "-MVC04-" )
+				                  .withVariant( variant )
+				                  .withFactory( factory )
 				                  .build();
-		Assert.assertNotNull(this.dataSource);
+		Assert.assertNotNull( this.dataSource );
 	}
 
 	@When("registering the data source on the manager")
 	public void registeringTheDataSourceOnTheManager() {
-		this.registeredDataSouce = DataSourceManager.registerDataSource(this.dataSource);
+		this.registeredDataSouce = DataSourceManager.registerDataSource( this.dataSource );
 	}
 
 	@Then("check that the header containers have the next data")
 	public void checkThatTheHeaderContainersHaveTheNextData( final List<Map<String, String>> cucumberTable ) {
-		final String controllerClass = cucumberTable.get(0).get(CONTROLLER_CLASS);
-		Assert.assertEquals("SeparatorController", controllerClass);
-		final String controllerModelTitle = cucumberTable.get(0).get(CONTROLLER_MODEL_TITLE);
+		final String controllerClass = cucumberTable.get( 0 ).get( CONTROLLER_CLASS );
+		Assert.assertEquals( "SeparatorController", controllerClass );
+		final String controllerModelTitle = cucumberTable.get( 0 ).get( CONTROLLER_MODEL_TITLE );
 //		Assert.assertEquals("SeparatorController", controllerModelTitle);
 	}
 
@@ -73,11 +69,39 @@ public class MVC04RunDataSource {
 	public void checkThatTheHeaderHasTheNextListOfControllers( final List<Map<String, String>> cucumberTable ) {
 		this.dataSource.collaborate2Model();
 		final List<IAndroidController> controllers = this.dataSource.getHeaderSectionContents();
-		Assert.assertNotNull(controllers);
-		Assert.assertEquals(1, controllers.size());
-		Assert.assertEquals(controllers.get(0).getModel().getClass().getSimpleName(),
-		                    cucumberTable.get(0).get(CONTROLLER_CLASS));
-		Assert.assertEquals(((Spacer)controllers.get(0).getModel()).getLabel(),
-		                    cucumberTable.get(0).get(CONTROLLER_MODEL_TITLE));
+		Assert.assertNotNull( controllers );
+		Assert.assertEquals( 1, controllers.size() );
+		Assert.assertEquals( controllers.get( 0 ).getModel().getClass().getSimpleName(),
+				cucumberTable.get( 0 ).get( CONTROLLER_CLASS ) );
+		Assert.assertEquals( ((Spacer) controllers.get( 0 ).getModel()).getLabel(),
+				cucumberTable.get( 0 ).get( CONTROLLER_MODEL_TITLE ) );
+	}
+}
+
+final class MVC04DataSource extends MVCDataSource {
+
+	@Override
+	public void prepareModel() throws Exception {
+
+	}
+
+	@Override
+	public void collaborate2Model() {
+
+	}
+
+	public static class Builder extends MVCDataSource.Builder<MVC04DataSource, MVC04DataSource.Builder> {
+		private MVC04DataSource onConstruction;
+
+		@Override
+		protected MVC04DataSource getActual() {
+			if (null == this.onConstruction) this.onConstruction = new MVC04DataSource();
+			return this.onConstruction;
+		}
+
+		@Override
+		protected Builder getActualBuilder() {
+			return this;
+		}
 	}
 }
