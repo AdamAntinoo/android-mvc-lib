0c455b6101c070f0ffe3e24ec92ba9377558e356
Adam Antinoo <adamantinoo.git@gmail.com>
2020-04-17 16:25:49 +0200

    -[SAVE] Save file modifications on removable branch.

--- AndroidMVC/src/main/java/org/dimensinfin/android/mvc/render/MVCExpandableRender.java
+++ AndroidMVC/src/main/java/org/dimensinfin/android/mvc/render/MVCExpandableRender.java
@@ -6,89 +6,94 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
+
+import java.util.Objects;
 
 import org.dimensinfin.android.mvc.R;
 import org.dimensinfin.android.mvc.controller.ExpandAndroidController;
 import org.dimensinfin.core.interfaces.IExpandable;
-import org.dimensinfin.core.interfaces.ICollaboration;
-
-import androidx.annotation.NonNull;
+import org.dimensinfin.logging.LogWrapper;
 
 /**
  * @author Adam Antinoo
  * @since 2.0.0
  */
 public abstract class MVCExpandableRender extends MVCRender {
-    // - L A Y O U T   F I E L D S
-    protected ImageView _rightArrow = null;
+	// - L A Y O U T   F I E L D S
+	protected ImageView _rightArrow;
 
-    protected ImageView nodeIcon = null;
-    protected TextView name = null;
-    protected TextView childCount = null;
+	protected ImageView nodeIcon;
+	protected TextView name;
+	protected TextView childCount;
 
-    // - L A Y O U T   L A B E L S
-    protected TextView classLabel = null;
-    protected TextView categoryLabel = null;
+	// - L A Y O U T   L A B E L S
+	protected TextView classLabel;
+	protected TextView categoryLabel;
 
-    // - C O N S T R U C T O R - S E C T I O N
-    public MVCExpandableRender( @NonNull final ExpandAndroidController controller, @NonNull final Context context) {
-        super(controller, context);
-    }
+	// - C O N S T R U C T O R - S E C T I O N
+	public MVCExpandableRender( @NonNull final ExpandAndroidController controller, @NonNull final Context context ) {
+		super( controller, context );
+	}
 
-    private ExpandAndroidController getExpandController() {
-        return (ExpandAndroidController) super.getController();
-    }
+	private ExpandAndroidController getExpandController() {
+		return (ExpandAndroidController) super.getController();
+	}
 
-    @Override
-    public void initializeViews() {
-        logger.info(">< [MVCExpandableRender.initializeViews]");
-        _rightArrow = this.getView().findViewById(R.id.rightArrow);
+	@Override
+	public int accessLayoutReference() {
+		return R.layout.baseexpandablenode;
+	}
 
-        nodeIcon = this.getView().findViewById(R.id.nodeIcon);
-        name = this.getView().findViewById(R.id.name);
-        childCount = this.getView().findViewById(R.id.childCount);
+	@Override
+	public void initializeViews() {
+		logger.info( ">< [MVCExpandableRender.initializeViews]" );
+		this._rightArrow = this.getView().findViewById( R.id.rightArrow );
+		this._rightArrow.setVisibility( View.GONE );
 
-        classLabel = this.getView().findViewById(R.id.classLabel);
-        categoryLabel = this.getView().findViewById(R.id.categoryLabel);
-    }
+		this.nodeIcon = Objects.requireNonNull( this.getView().findViewById( R.id.nodeIcon ) );
+		this.name = Objects.requireNonNull( this.getView().findViewById( R.id.name ) );
+		this.childCount = Objects.requireNonNull( this.getView().findViewById( R.id.childCount ) );
+		this.nodeIcon.setVisibility( View.GONE );
+		this.name.setVisibility( View.INVISIBLE );
+		this.childCount.setVisibility( View.INVISIBLE );
 
-    /**
-     * Check if the expandable nodes have a click active. During the click show the spinner until the action finishes with
-     * a new <code>updateContent()</code>.
-     */
-    @Override
-    public void updateContent() {
-        logger.info(">< [MVCExpandableRender.updateContent]");
-        // Check if the model is expandable to show or hide the arrow.
-        ICollaboration targetModel = (ICollaboration) this.getController().getModel();
-        if (null != _rightArrow) {
-            if (targetModel instanceof IExpandable) {
-                _rightArrow.setVisibility(View.VISIBLE);
-                logger.info(">< [MVCExpandableRender.updateContent]> Setting right arrow");
-                if (((IExpandable) targetModel).isExpanded()) {
-                    _rightArrow.setImageResource(R.drawable.arrowdown);
-                } else {
-                    _rightArrow.setImageResource(R.drawable.arrowright);
-                }
+		classLabel = this.getView().findViewById( R.id.classLabel );
+		categoryLabel = this.getView().findViewById( R.id.categoryLabel );
+	}
 
-                // Check if the click is running and then replace the right arrow by an spinner.
-                if (this.getExpandController().clickRunning()) {
-                    if (null != _rightArrow) {
-                        logger.info("-- [MVCExpandableRender.updateContent]> Activating spinner animation.");
-                        _rightArrow.setImageDrawable(getDrawable(R.drawable.progress_spinner_orange));
-                        Animation rotation = AnimationUtils.loadAnimation(getContext(), R.anim.clockwise_rotation);
-                        rotation.setRepeatCount(Animation.INFINITE);
-                        _rightArrow.startAnimation(rotation);
-                    }
-                }
-            } else _rightArrow.setVisibility(View.GONE);
-        }
-    }
+	/**
+	 * Check if the expandable nodes have a click active. During the click show the spinner until the action finishes with
+	 * a new <code>updateContent()</code>.
+	 */
+	@Override
+	public void updateContent() {
+		LogWrapper.enter();
+		// Check if the model is expandable to show or hide the arrow.
+		ICollaboration targetModel = (ICollaboration) this.getController().getModel();
+		if (null != _rightArrow) {
+			if (targetModel instanceof IExpandable) {
+				_rightArrow.setVisibility( View.VISIBLE );
+                LogWrapper.info( "Setting right arrow" );
+				if (((IExpandable) targetModel).isExpanded()) {
+					this._rightArrow.setImageResource( R.drawable.arrowdown );
+				} else {
+					this._rightArrow.setImageResource( R.drawable.arrowright );
+				}
 
-    @Override
-    public int accessLayoutReference() {
-        return R.layout.baseexpandablenode;
-    }
+				// Check if the click is running and then replace the right arrow by an spinner.
+				if (this.getExpandController().clickRunning()) {
+//					if (null != _rightArrow) {
+                    LogWrapper.info( "Activating spinner animation." );
+						this._rightArrow.setImageDrawable( getDrawable( R.drawable.progress_spinner_orange ) );
+						Animation rotation = AnimationUtils.loadAnimation( getContext(), R.anim.clockwise_rotation );
+						rotation.setRepeatCount( Animation.INFINITE );
+						this._rightArrow.startAnimation( rotation );
+//					}
+				}
+			} else this._rightArrow.setVisibility( View.GONE );
+		}
+	}
 
 //	//--- D E L E G A T E D   M E T H O D S
 //	@Override
